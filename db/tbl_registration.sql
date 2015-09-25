26039ec1ac77145758b65b079f7a2c7982d5a10b
Ali Fahmi PN <lab@advanced-network.org>
2015-09-25 22:25:59 +0700

    DB TBL REGISTRATION

--- db/tbl_registration.sql
+++ db/tbl_registration.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 22, 2015 at 08:04 AM
+-- Generation Time: Sep 25, 2015 at 03:25 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.27
 
@@ -23,26 +23,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `registration`
+-- Table structure for table `tbl_registration`
 --
 
-CREATE TABLE IF NOT EXISTS `registration` (
+CREATE TABLE IF NOT EXISTS `tbl_registration` (
   `registration_id` int(11) NOT NULL,
-  `registration_name` int(50) NOT NULL,
-  `registration_email` int(50) NOT NULL,
+  `registration_name` varchar(50) NOT NULL,
+  `registration_age` varchar(2) NOT NULL,
+  `registration_address` text NOT NULL,
+  `registration_phone` varchar(16) NOT NULL,
+  `registration_email` varchar(50) DEFAULT NULL,
+  `registration_image_dir` varchar(255) NOT NULL,
   `registration_idcard` varchar(255) NOT NULL,
-  `registration_image` varchar(255) NOT NULL,
+  `registration_photo` varchar(255) NOT NULL,
   `registration_created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_registration`
+--
+
+INSERT INTO `tbl_registration` (`registration_id`, `registration_name`, `registration_age`, `registration_address`, `registration_phone`, `registration_email`, `registration_image_dir`, `registration_idcard`, `registration_photo`, `registration_created_at`) VALUES
+(1, 'Ali Fahmi', '29', 'Jalan Prapanca Raya 12 LG Kebayoran baru Jaksel 12150', '082233043298', NULL, '25-09-2015', 'id_nasihat.png', 'foto_nasihat.png', '2015-09-25 14:45:01'),
+(2, 'Agam Fauza', '23', 'Jalan Prapanca Raya 12 LG Kebayoran baru Jaksel 12150', '0852', NULL, '25-09-2015', 'id_not_resign_reasons.png', 'foto_not_resign_reasons.png', '2015-09-25 15:24:33');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `registration`
+-- Indexes for table `tbl_registration`
 --
-ALTER TABLE `registration`
+ALTER TABLE `tbl_registration`
   ADD PRIMARY KEY (`registration_id`);
 
 --
@@ -50,10 +62,10 @@ ALTER TABLE `registration`
 --
 
 --
--- AUTO_INCREMENT for table `registration`
+-- AUTO_INCREMENT for table `tbl_registration`
 --
-ALTER TABLE `registration`
-  MODIFY `registration_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tbl_registration`
+  MODIFY `registration_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
