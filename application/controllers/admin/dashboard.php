265430407c14bd8c4af47cfb159861a5c8054eea
Ali Fahmi PN <lab@advanced-network.org>
2015-12-11 16:28:50 +0700

    Add 50 Foto + Paginantion 20

--- application/controllers/admin/dashboard.php
+++ application/controllers/admin/dashboard.php
@@ -14,7 +14,7 @@ class Dashboard extends CI_Controller {
       if( $this->session->userdata('logged_in') )
       {
           //Pagination
-          $config['per_page']     = 4;
+          $config['per_page']     = 48;
 
           if($fave=='active'){
             //Pagination
@@ -52,25 +52,25 @@ class Dashboard extends CI_Controller {
 
           $config['suffix']           = '';
           $config['first_url']        = $config['base_url'] . $config['suffix'];
-          $config['num_links']        = 2;
-          $config['full_tag_open'] = '<ul class="pagination">';
-          $config['full_tag_close'] = '</ul>';
-          $config['first_link'] = false;
-          $config['last_link'] = false;
-          $config['first_tag_open'] = '<li>';
-          $config['first_tag_close'] = '</li>';
-          $config['prev_link'] = '&laquo';
-          $config['prev_tag_open'] = '<li class="prev">';
-          $config['prev_tag_close'] = '</li>';
-          $config['next_link'] = '&raquo';
-          $config['next_tag_open'] = '<li>';
-          $config['next_tag_close'] = '</li>';
-          $config['last_tag_open'] = '<li>';
-          $config['last_tag_close'] = '</li>';
-          $config['cur_tag_open'] = '<li class="active"><a href="#">';
-          $config['cur_tag_close'] = '</a></li>';
-          $config['num_tag_open'] = '<li>';
-          $config['num_tag_close'] = '</li>';
+          $config['num_links']        = 20;
+          $config['full_tag_open']    = '<ul class="pagination">';
+          $config['full_tag_close']   = '</ul>';
+          $config['first_link']       = false;
+          $config['last_link']        = false;
+          $config['first_tag_open']   = '<li>';
+          $config['first_tag_close']  = '</li>';
+          $config['prev_link']        = '&laquo';
+          $config['prev_tag_open']    = '<li class="prev">';
+          $config['prev_tag_close']   = '</li>';
+          $config['next_link']        = '&raquo';
+          $config['next_tag_open']    = '<li>';
+          $config['next_tag_close']   = '</li>';
+          $config['last_tag_open']    = '<li>';
+          $config['last_tag_close']   = '</li>';
+          $config['cur_tag_open']     = '<li class="active"><a href="#">';
+          $config['cur_tag_close']    = '</a></li>';
+          $config['num_tag_open']     = '<li>';
+          $config['num_tag_close']    = '</li>';
 
           $this->pagination->initialize($config);
 
